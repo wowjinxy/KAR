1dbbad45ea51d10d413629b076966086af177341
wowjinxy <1340990+wowjinxy@users.noreply.github.com>
2026-06-16 19:39:24 -0500

    Add vcdevice wrapper functions

--- src/kar/vc/vcdevice.c
+++ src/kar/vc/vcdevice.c
@@ -53,6 +53,43 @@ extern const f32 lbl_805E1EE0;
 extern const f32 lbl_805E2064;
 
 extern void HSD_GObjDestroy(HSD_GObj* gobj);
+extern void fn_801E1DC0(VcDevice* device);
+extern void fn_801E1E6C(VcDevice* device);
+extern void fn_801E441C(VcDevice* device);
+extern void fn_801E45CC(VcDevice* device);
+extern void fn_801E6670(VcDevice* device);
+extern void fn_801E6D24(VcDevice* device);
+extern void fn_801E7154(VcDevice* device);
+extern void fn_801FA188(VcDevice* device);
+extern void fn_801FA1E8(VcDevice* device);
+extern void fn_801FA2BC(VcDevice* device);
+extern void kar_mpcoll__near_801cc378(VcDevice* device);
+extern void kar_mpcoll__near_801cc480(VcDevice* device);
+extern void kar_mpcoll__near_801cd658(VcDevice* device);
+extern void kar_mpcoll__near_801cd71c(VcDevice* device);
+extern void kar_mpcoll__near_801cdb8c(VcDevice* device);
+extern void kar_mpcoll__near_801ce5b4(VcDevice* device);
+extern void kar_mpcoll__near_801ce650(VcDevice* device);
+extern void kar_mpcoll__near_801d04d8(VcDevice* device);
+extern void kar_mpcoll__near_801edd20(VcDevice* device);
+extern void kar_mpcoll__near_801eddd0(VcDevice* device);
+extern void kar_mpcoll__near_801ef454(VcDevice* device);
+extern void kar_mpcoll__near_801f7e54(VcDevice* device);
+extern void kar_mpcoll__near_801f8578(VcDevice* device);
+extern void kar_mpcoll__near_801f8ef4(VcDevice* device);
+extern void kar_mpcoll__near_801f8f44(VcDevice* device);
+extern void kar_mpcoll__near_801f913c(VcDevice* device);
+extern void kar_mpcoll__near_801f9160(VcDevice* device);
+extern void kar_mpcoll__near_801f9184(VcDevice* device);
+extern void kar_mpcoll__near_801f94a0(VcDevice* device);
+extern void kar_mpcoll__near_801f94c4(VcDevice* device);
+extern void kar_mpcoll__near_801f9594(VcDevice* device);
+extern void kar_mpcoll__near_801f9738(VcDevice* device);
+extern void kar_mpcoll__near_801f9778(VcDevice* device);
+extern void kar_mpcoll__near_801f97e4(VcDevice* device);
+extern void kar_mpcoll__near_801f99e0(VcDevice* device);
+extern void kar_mpcoll__near_801f9a04(VcDevice* device);
+extern void kar_mpcoll__near_801f9a6c(VcDevice* device);
 
 void fn_801E57FC(VcDeviceDestroyData* data)
 {
@@ -155,9 +192,12 @@ void fn_801EEF78(void)
 {
 }
 
+#pragma push
+#pragma dont_inline on
 void kar_mpcoll__near_801ef2f4(void)
 {
 }
+#pragma pop
 
 void fn_801EFC98(void)
 {
@@ -318,9 +358,12 @@ void fn_801F9EA0(void)
 {
 }
 
+#pragma push
+#pragma dont_inline on
 void fn_801FA184(void)
 {
 }
+#pragma pop
 
 BOOL fn_801FA064(VcDevice* device)
 {
@@ -359,3 +402,291 @@ VcDevice* fn_801FC294(HSD_GObj* gobj)
 {
     return gobj->user_data;
 }
+
+#pragma push
+#pragma dont_inline on
+void fn_801E6828(VcDevice* device)
+{
+    kar_mpcoll__near_801ce650(device);
+}
+
+void fn_801E6EB4(VcDevice* device)
+{
+    kar_mpcoll__near_801ce5b4(device);
+}
+
+void fn_801E6F98(VcDevice* device)
+{
+    kar_mpcoll__near_801ce650(device);
+}
+
+void fn_801EB95C(VcDevice* device)
+{
+    kar_mpcoll__near_801cc378(device);
+}
+
+void kar_mpcoll__near_801edd00(VcDevice* device)
+{
+    kar_mpcoll__near_801cd658(device);
+}
+
+void kar_mpcoll__near_801eddb0(VcDevice* device)
+{
+    kar_mpcoll__near_801cdb8c(device);
+}
+
+void kar_mpcoll__near_801eecb0(VcDevice* device)
+{
+    fn_801E1E6C(device);
+}
+
+void kar_mpcoll__near_801eedf8(void)
+{
+    kar_mpcoll__near_801ef2f4();
+}
+
+void kar_mpcoll__near_801eee94(VcDevice* device)
+{
+    kar_mpcoll__near_801edd20(device);
+}
+
+void fn_801EF74C(VcDevice* device)
+{
+    kar_mpcoll__near_801ef454(device);
+}
+
+void fn_801F03C4(VcDevice* device)
+{
+    fn_801E441C(device);
+}
+
+void fn_801F03E4(VcDevice* device)
+{
+    fn_801E45CC(device);
+}
+
+void fn_801F04C4(VcDevice* device)
+{
+    fn_801E6670(device);
+}
+
+void fn_801F059C(VcDevice* device)
+{
+    fn_801E6828(device);
+}
+
+void fn_801F0844(void)
+{
+    kar_mpcoll__near_801ef2f4();
+}
+
+void fn_801F0A34(VcDevice* device)
+{
+    fn_801E7154(device);
+}
+
+void fn_801F0AC8(VcDevice* device)
+{
+    kar_mpcoll__near_801eddd0(device);
+}
+
+void fn_801F0BB0(VcDevice* device)
+{
+    fn_801E6D24(device);
+}
+
+void fn_801F0BD0(VcDevice* device)
+{
+    fn_801E6EB4(device);
+}
+
+void fn_801F0C4C(VcDevice* device)
+{
+    fn_801E6F98(device);
+}
+
+void fn_801F5F24(VcDevice* device)
+{
+    kar_mpcoll__near_801cc378(device);
+}
+
+void kar_mpcoll__near_801f8518(VcDevice* device)
+{
+    kar_mpcoll__near_801cd71c(device);
+}
+
+void kar_mpcoll__near_801f8538(VcDevice* device)
+{
+    kar_mpcoll__near_801d04d8(device);
+}
+
+void kar_mpcoll__near_801f8558(VcDevice* device)
+{
+    kar_mpcoll__near_801cdb8c(device);
+}
+
+void kar_mpcoll__near_801f8a2c(VcDevice* device)
+{
+    kar_mpcoll__near_801f7e54(device);
+}
+
+void kar_mpcoll__near_801f8e6c(VcDevice* device)
+{
+    kar_mpcoll__near_801ce650(device);
+}
+
+void kar_mpcoll__near_801f8ed4(VcDevice* device)
+{
+    fn_801E1E6C(device);
+}
+
+void kar_mpcoll__near_801f8f24(VcDevice* device)
+{
+    fn_801E1DC0(device);
+}
+
+void kar_mpcoll__near_801f9058(VcDevice* device)
+{
+    kar_mpcoll__near_801f913c(device);
+}
+
+void kar_mpcoll__near_801f9078(VcDevice* device)
+{
+    kar_mpcoll__near_801f9160(device);
+}
+
+void kar_mpcoll__near_801f9098(VcDevice* device)
+{
+    kar_mpcoll__near_801f9184(device);
+}
+
+void kar_mpcoll__near_801f911c(void)
+{
+    fn_801FA184();
+}
+
+void kar_mpcoll__near_801f9258(VcDevice* device)
+{
+    kar_mpcoll__near_801f8ef4(device);
+}
+
+void kar_mpcoll__near_801f9278(VcDevice* device)
+{
+    kar_mpcoll__near_801f8f24(device);
+}
+
+void kar_mpcoll__near_801f9298(VcDevice* device)
+{
+    kar_mpcoll__near_801f8f44(device);
+}
+
+void kar_mpcoll__near_801f935c(VcDevice* device)
+{
+    fn_801E6670(device);
+}
+
+void kar_mpcoll__near_801f9434(VcDevice* device)
+{
+    fn_801E6828(device);
+}
+
+void kar_mpcoll__near_801f990c(VcDevice* device)
+{
+    kar_mpcoll__near_801f99e0(device);
+}
+
+void kar_mpcoll__near_801f992c(VcDevice* device)
+{
+    kar_mpcoll__near_801f9a04(device);
+}
+
+void kar_mpcoll__near_801f994c(VcDevice* device)
+{
+    kar_mpcoll__near_801f9a6c(device);
+}
+
+void kar_mpcoll__near_801f9b64(VcDevice* device)
+{
+    kar_mpcoll__near_801f9738(device);
+}
+
+void fn_801F9B84(VcDevice* device)
+{
+    kar_mpcoll__near_801f9778(device);
+}
+
+void fn_801F9BA4(VcDevice* device)
+{
+    kar_mpcoll__near_801f97e4(device);
+}
+
+void fn_801F9CA4(VcDevice* device)
+{
+    fn_801E7154(device);
+}
+
+void fn_801F9D20(VcDevice* device)
+{
+    kar_mpcoll__near_801f8578(device);
+}
+
+void fn_801F9E08(VcDevice* device)
+{
+    fn_801E6D24(device);
+}
+
+void fn_801F9E28(VcDevice* device)
+{
+    fn_801E6EB4(device);
+}
+
+void fn_801F9EA4(VcDevice* device)
+{
+    fn_801E6F98(device);
+}
+
+void fn_801FA004(VcDevice* device)
+{
+    fn_801FA188(device);
+}
+
+void fn_801FA024(VcDevice* device)
+{
+    fn_801FA1E8(device);
+}
+
+void fn_801FA044(VcDevice* device)
+{
+    fn_801FA2BC(device);
+}
+
+void fn_801FA1C8(VcDevice* device)
+{
+    kar_mpcoll__near_801cc480(device);
+}
+
+void fn_801FA5C0(VcDevice* device)
+{
+    fn_801FA2BC(device);
+}
+
+void fn_801FA6B0(VcDevice* device)
+{
+    kar_mpcoll__near_801f94a0(device);
+}
+
+void fn_801FA6D0(VcDevice* device)
+{
+    kar_mpcoll__near_801f94c4(device);
+}
+
+void fn_801FA6F0(VcDevice* device)
+{
+    kar_mpcoll__near_801f9594(device);
+}
+
+void fn_801FB324(VcDevice* device)
+{
+    fn_801E45CC(device);
+}
+#pragma pop
