891ab429081251090553c0844a2b2710df1665d0
DeDeDeK <216604342+DeDeDek@users.noreply.github.com>
2026-07-06 15:19:30 -0600

    Add SysDolphin perf source

---
+++ include/sysdolphin/perf.h
@@ -0,0 +1,22 @@
+#ifndef _hsd_perf_h_
+#define _hsd_perf_h_
+
+#include <global.h>
+#include <dolphin/types.h>
+
+typedef struct _HSD_PerfStat {
+    f32 cpu_time;
+    f32 draw_time;
+    f32 total_time;
+    u32 nb_mtx_load;
+    u32 env_blend[32];
+} HSD_PerfStat;
+
+void HSD_PerfInitStat(void);
+void HSD_PerfSetStartTime(void);
+void HSD_PerfSetCPUTime(void);
+void HSD_PerfSetDrawTime(void);
+void HSD_PerfSetTotalTime(void);
+void HSD_PerfCountEnvelopeBlending(s32 n);
+
+#endif
