328319a9078479230dc8604f0442f92ae014ceba
DeDeDeK <216604342+DeDeDek@users.noreply.github.com>
2026-07-07 12:26:48 -0600

    Add SysDolphin mtx source

--- include/sysdolphin/mtx.h
+++ include/sysdolphin/mtx.h
@@ -2,31 +2,34 @@
 #define _mtx_h_
 
 #include <global.h>
+#include <dolphin/mtx/mtxtypes.h>
 #include <sysdolphin/objalloc.h>
-#include <math.h>
 
-void func_80379310(Mtx src, Mtx dest);
+void HSD_MtxInverse(Mtx src, Mtx dest);
 void HSD_MtxInverseConcat(Mtx inv, Mtx src, Mtx dest);
-void func_80379A20(Mtx src, Mtx dest);
-void func_80379C24(Mtx m, Vec* vec);
-void func_80379F6C(Mtx mat, Vec* vec);
-void func_80379F88(Mtx arg0, Vec* arg1);
-void func_8037A120(Mtx arg0, Vec* arg1);
-void func_8037A230(Mtx arg0, Quaternion* arg1);
-void func_8037A250(Mtx m, Vec* vec1, Vec* vec2, Vec* vec3, Vec* vec4);
-void func_8037A43C(Mtx arg0, Vec* arg1, Quaternion* arg2, Vec* arg3, Vec* arg4);
-void func_8037A54C(Mtx arg0, Mtx arg1, Mtx arg2, f32 arg3);
-void* func_8037A610(void);
-void func_8037A65C(void* arg0);
-void* func_8037A68C(void);
-void func_8037A6D8(void* arg0);
+void HSD_MtxGetRotation(Mtx m, Vec* vec);
+void HSD_MtxGetRotationMtx(Mtx v0, Mtx dest, char axis0, char axis1);
+void HSD_MtxGetTranslate(Mtx mat, Vec* vec);
+void HSD_MtxGetScale(Mtx arg0, Vec* arg1);
+void HSD_MkRotationMtx(Mtx arg0, Vec* arg1);
+void HSD_MtxSRT(Mtx m, Vec* vec1, Vec* vec2, Vec* vec3, Vec* vec4);
+void HSD_MtxSRTQuat(Mtx arg0, Vec* arg1, Quaternion* arg2, Vec* arg3,
+                     Vec* arg4);
+void HSD_MtxScaledAdd(Mtx arg0, Mtx arg1, Mtx arg2, f32 arg3);
+void HSD_VecGetOrthogonal(Vec* v, Vec* out);
+Vec* HSD_VecAlloc(void);
+void HSD_VecFree(Vec* arg0);
+void* HSD_MtxAlloc(void);
+void HSD_MtxFree(void* arg0);
 HSD_ObjAllocData* HSD_VecGetAllocData(void);
 void HSD_VecInitAllocData(void);
 HSD_ObjAllocData* HSD_MtxGetAllocData(void);
 void HSD_MtxInitAllocData(void);
 
-extern float func_80022DBC(float);
-extern float func_someCalcAngle_80022C30(float, float);
+s32 QuatMul(Quaternion* p, Quaternion* q, Quaternion* out);
+s32 QuatSetUp(Vec* axis, Quaternion* q, f32 angle);
+s32 EulerToQuat(Vec* euler, Quaternion* q);
+s32 QuatSlerp(Quaternion* p, Quaternion* q, Quaternion* out, f32 t);
 
 inline f32 fabsf_bitwise(f32 v)
 {
